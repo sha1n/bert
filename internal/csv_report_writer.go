96832d8bc7ab97b9f0a8e1d1024f485a1d79dd3c
Shai Nagar <shain@wix.com>
2021-05-18 00:53:14 +0300

    changed csv record layout

--- internal/csv_report_writer.go
+++ internal/csv_report_writer.go
@@ -25,7 +25,7 @@ func NewCsvReportWriter(writer *bufio.Writer) api.WriteReportFn {
 }
 
 func (rw *csvReportWriter) Write(summary api.Summary, config *api.BenchmarkSpec) (err error) {
-	rw.writer.Write([]string{"Timestamp", "Scenario", "Stat", "Value"})
+	rw.writer.Write([]string{"Timestamp", "Scenario", "Min", "Max", "Mean", "Median", "Percentile 90", "StdDev", "Errors"})
 
 	timeStr := summary.Time().Format("2006-01-02T15:04:05Z07:00")
 	sortedIds := getSortedScenarioIds(summary)
@@ -34,13 +34,18 @@ func (rw *csvReportWriter) Write(summary api.Summary, config *api.BenchmarkSpec)
 		id := sortedIds[i]
 		stats := summary.Get(id)
 
-		rw.writeStatNano(timeStr, id, "min", stats.Min)
-		rw.writeStatNano(timeStr, id, "max", stats.Max)
-		rw.writeStatNano(timeStr, id, "mean", stats.Mean)
-		rw.writeStatNano(timeStr, id, "median", stats.Median)
-		rw.writeStatNano(timeStr, id, "p90", func() (float64, error) { return stats.Percentile(90) })
-		rw.writeStatNano(timeStr, id, "stddev", stats.StdDev)
-		rw.writeErrorRateStat(timeStr, id, "errors", stats.ErrorRate)
+		rw.writer.Write([]string{
+			timeStr,
+			id,
+			rw.writeStatNano(stats.Min),
+			rw.writeStatNano(stats.Max),
+			rw.writeStatNano(stats.Mean),
+			rw.writeStatNano(stats.Median),
+			rw.writeStatNano(func() (float64, error) { return stats.Percentile(90) }),
+			rw.writeStatNano(stats.StdDev),
+			rw.writeErrorRateStat(stats.ErrorRate),
+		})
+
 		rw.writer.Flush()
 	}
 
@@ -58,22 +63,18 @@ func getSortedScenarioIds(summary api.Summary) []api.ID {
 	return sortedIds
 }
 
-func (rw *csvReportWriter) writeStatNano(timeStr string, id string, name string, f func() (float64, error)) {
+func (rw *csvReportWriter) writeStatNano(f func() (float64, error)) string {
 	value, err := f()
 	if err == nil {
-		rw.writer.Write([]string{timeStr, id, name, fmt.Sprintf("%.3f", value)})
-	} else {
-		rw.writeStatError(id, name)
+		return fmt.Sprintf("%.3f", value)
 	}
+
+	return "ERROR"
 }
 
-func (rw *csvReportWriter) writeErrorRateStat(timeStr string, id string, name string, f func() float64) {
+func (rw *csvReportWriter) writeErrorRateStat(f func() float64) string {
 	value := f()
 	errorRate := int(value * 100)
 
-	rw.writer.Write([]string{timeStr, id, name, fmt.Sprintf("%d", errorRate)})
-}
-
-func (rw *csvReportWriter) writeStatError(id string, name string) {
-	rw.writer.Write([]string{id, name, "ERR"})
+	return fmt.Sprintf("%d", errorRate)
 }
--- internal/csv_report_writer_test.go
+++ internal/csv_report_writer_test.go
@@ -4,8 +4,7 @@ import (
 	"bufio"
 	"bytes"
 	"encoding/csv"
-	"sort"
-	"strconv"
+	"fmt"
 	"testing"
 
 	"github.com/sha1n/benchy/api"
@@ -13,24 +12,47 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-const ExpectedLinesPerSubject = 7
-
 func TestWrite(t *testing.T) {
 	var scenario1, scenario2 = scenario{id: "1-id"}, scenario{id: "2-id"}
 	summary := aSummaryFor(scenario1, scenario2)
 
 	allRecords := writeCsvReport(t, summary)
 
-	assert.Equal(t, 1+2*ExpectedLinesPerSubject, len(allRecords))
+	assert.Equal(t, 1+2, len(allRecords))
 
 	// Titles
-	assert.Equal(t, []string{"Timestamp", "Scenario", "Stat", "Value"}, allRecords[0])
+	assert.Equal(t, []string{"Timestamp", "Scenario", "Min", "Max", "Mean", "Median", "Percentile 90", "StdDev", "Errors"}, allRecords[0])
 
 	expectedTimestamp := summary.Time().Format("2006-01-02T15:04:05Z07:00")
-	// scenario 1
-	assertRecordRange(t, 1, "1-id", expectedTimestamp, allRecords)
-	// scenario 2
-	assertRecordRange(t, 8, "2-id", expectedTimestamp, allRecords)
+
+	assertRecord(t, scenario1, summary, expectedTimestamp, allRecords[1])
+	assertRecord(t, scenario2, summary, expectedTimestamp, allRecords[2])
+}
+
+func assertRecord(t *testing.T, scenario api.Identifiable, summary api.Summary, expectedTimestamp string, actual []string) {
+	stats1 := summary.Get(scenario.ID())
+
+	assert.Equal(t, expectedTimestamp, actual[0])
+	assert.Equal(t, scenario.ID(), actual[1])
+	assert.Equal(t, expectedFloatFormat(stats1.Min), actual[2])
+	assert.Equal(t, expectedFloatFormat(stats1.Max), actual[3])
+	assert.Equal(t, expectedFloatFormat(stats1.Mean), actual[4])
+	assert.Equal(t, expectedFloatFormat(stats1.Median), actual[5])
+	assert.Equal(t, expectedFloatFormat(func() (float64, error) { return stats1.Percentile(90) }), actual[6])
+	assert.Equal(t, expectedFloatFormat(stats1.StdDev), actual[7])
+	assert.Equal(t, expectedRateFormat(stats1.ErrorRate), actual[8])
+}
+
+func expectedRateFormat(f func() float64) string {
+	errorRate := int(f() * 100)
+
+	return fmt.Sprintf("%d", errorRate)
+
+}
+
+func expectedFloatFormat(f func() (float64, error)) string {
+	v, _ := f()
+	return fmt.Sprintf("%.3f", v)
 }
 
 func writeCsvReport(t *testing.T, summary api.Summary) [][]string {
@@ -46,26 +68,6 @@ func writeCsvReport(t *testing.T, summary api.Summary) [][]string {
 	assert.NoError(t, err)
 
 	return allRecords
-}
-
-func assertRecordRange(t *testing.T, startIndex int, id string, expectedTimestamp string, records [][]string) {
-	var statNames []string
-	for i := startIndex; i < startIndex+ExpectedLinesPerSubject; i++ {
-		assert.Equal(t, 4, len(records[i]))
-		assert.Equal(t, expectedTimestamp, records[i][0])
-		assert.Equal(t, id, records[i][1])
-
-		_, err := strconv.ParseFloat(records[i][3], 64)
-		assert.NoError(t, err)
-
-		statNames = append(statNames, records[i][2])
-	}
-
-	expectedStatNames := []string{"errors", "max", "mean", "median", "min", "p90", "stddev"}
-	sort.Strings(expectedStatNames)
-	sort.Strings(statNames)
-
-	assert.Equal(t, expectedStatNames, statNames)
 }
 
 func aSummaryFor(i1 api.Identifiable, i2 api.Identifiable) api.Summary {
