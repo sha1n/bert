96ba98ebe012c49d3461cfa3e7c983f59dbf5d48
Shai Nagar <sha1n@users.noreply.github.com>
2021-05-20 18:13:13 +0300

    redesigned report engine (#27)
    added the ability to generate trace raw-data reports in streaming to prevent data lose in very long benchmark situations

---
+++ pkg/stream_report_handler_test.go
@@ -0,0 +1,60 @@
+package pkg
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/sha1n/benchy/api"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStreamReportHandler(t *testing.T) {
+	testStreamReportHandlerFinalizeWith(t, nil, nil)
+}
+
+func TestStreamReportHandlerFinalizeHandleError(t *testing.T) {
+	testStreamReportHandlerFinalizeWith(t, errors.New("test error"), nil)
+}
+
+func TestStreamReportHandlerFinalizeTraceError(t *testing.T) {
+	testStreamReportHandlerFinalizeWith(t, nil, errors.New("test error"))
+}
+
+func testStreamReportHandlerFinalizeWith(t *testing.T, expectedHandleError error, expectedTraceError error) {
+	tracer := NewTracer(1)
+	expectedSpec := exampleSpec()
+	expectedCtx := &api.ReportContext{}
+
+	interceptor := newHandleInterceptor(expectedHandleError)
+
+	handler := NewStreamReportHandler(expectedSpec, expectedCtx, interceptor.intercept)
+
+	handler.Subscribe(tracer.Stream())
+
+	// Fire one trace event
+	tracer.Start(expectedSpec.Scenarios[0])(expectedTraceError)
+
+	assert.NoError(t, handler.Finalize())
+
+	assert.Equal(t, expectedHandleError, interceptor.expectedError)
+	assert.Equal(t, expectedSpec.Scenarios[0].ID(), interceptor.capturedTrace.ID())
+	assert.Equal(t, expectedTraceError, interceptor.capturedTrace.Error())
+}
+
+type handleInterceptor struct {
+	capturedTrace api.Trace
+	expectedError error
+}
+
+func newHandleInterceptor(expectedError error) *handleInterceptor {
+	return &handleInterceptor{
+		expectedError: expectedError,
+	}
+}
+
+func (i *handleInterceptor) intercept(trace api.Trace) error {
+	i.capturedTrace = trace
+
+	return i.expectedError
+}
---
+++ pkg/summary_report_handler.go
@@ -0,0 +1,34 @@
+package pkg
+
+import (
+	"github.com/sha1n/benchy/api"
+)
+
+type summaryReportHandler struct {
+	spec          *api.BenchmarkSpec
+	ctx           *api.ReportContext
+	sink          *TraceSink
+	unsubscribe   Unsubscribe
+	writeReportFn api.WriteReportFn
+}
+
+// NewSummaryReportHandler create summary report subscriber
+// Summary report handlers typically need to accumulate all the data in order to generate a report.
+func NewSummaryReportHandler(spec *api.BenchmarkSpec, ctx *api.ReportContext, writeReportFn api.WriteReportFn) api.ReportHandler {
+	return &summaryReportHandler{
+		spec:          spec,
+		ctx:           ctx,
+		writeReportFn: writeReportFn,
+	}
+}
+
+func (h *summaryReportHandler) Subscribe(stream api.TraceStream) {
+	h.sink = NewTraceSink(stream)
+	h.unsubscribe = h.sink.Subscribe()
+}
+
+func (h *summaryReportHandler) Finalize() error {
+	h.unsubscribe()
+
+	return h.writeReportFn(h.sink.Summary(), h.spec, h.ctx)
+}
