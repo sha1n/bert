80ad10985daf303375a801f61be7a3957e1873fe
sha1n <sha1n@users.noreply.github.com>
2021-05-22 11:03:47 +0300

    improved input error handling

--- internal/cli/runner.go
+++ internal/cli/runner.go
@@ -5,46 +5,12 @@ import (
 	"errors"
 	"fmt"
 	"os"
-	"path/filepath"
-	"strings"
 
 	"github.com/sha1n/benchy/api"
 	internal "github.com/sha1n/benchy/internal/report"
 	"github.com/sha1n/benchy/pkg"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
-)
-
-const (
-	// ArgNameConfig : program arg name
-	ArgNameConfig = "config"
-	// ArgNameOutputFile : program arg name
-	ArgNameOutputFile = "out-file"
-	// ArgNameFormat : program arg name
-	ArgNameFormat = "format"
-	// ArgNamePipeStdout : program arg name
-	ArgNamePipeStdout = "pipe-stdout"
-	// ArgNamePipeStderr : program arg name
-	ArgNamePipeStderr = "pipe-stderr"
-	// ArgNameDebug : program arg name
-	ArgNameDebug = "debug"
-	// ArgNameSilent : program arg name
-	ArgNameSilent = "silent"
-	// ArgNameLabel : program arg name
-	ArgNameLabel = "label"
-	// ArgNameHeaders : program arg name
-	ArgNameHeaders = "headers"
-
-	// ArgValueReportFormatTxt : Plain text report format arg value
-	ArgValueReportFormatTxt = "txt"
-	// ArgValueReportFormatCsv : CSV report format arg value
-	ArgValueReportFormatCsv = "csv"
-	// ArgValueReportFormatCsvRaw : CSV raw data report format value
-	ArgValueReportFormatCsvRaw = "csv/raw"
-	// ArgValueReportFormatMarkdown : Markdown report format arg value
-	ArgValueReportFormatMarkdown = "md"
-	// ArgValueReportFormatMarkdownRaw : Markdown report format arg value
-	ArgValueReportFormatMarkdownRaw = "md/raw"
 )
 
 func init() {
@@ -73,22 +39,15 @@ func Run(cmd *cobra.Command, args []string) {
 		err = reportHandler.Finalize()
 	}
 
-	checkFatal(err)
-}
-
-func checkFatal(err error) {
-	if err != nil {
-		log.Errorf("Failed to execute benchark. Error: %s", err.Error())
-		log.Exit(1)
-	}
+	CheckInitFatal(err)
 }
 
 func configureLogger(cmd *cobra.Command) {
-	silent, _ := cmd.Flags().GetBool(ArgNameSilent)
-	debug, _ := cmd.Flags().GetBool(ArgNameDebug)
+	silent := GetBool(cmd, ArgNameSilent)
+	debug := GetBool(cmd, ArgNameDebug)
 
 	if silent && debug {
-		checkFatal(errors.New("'--%s' and '--%s' are mutually exclusive"))
+		CheckInitFatal(errors.New("'--%s' and '--%s' are mutually exclusive"))
 	}
 	if silent {
 		log.StandardLogger().SetLevel(log.PanicLevel)
@@ -99,19 +58,19 @@ func configureLogger(cmd *cobra.Command) {
 }
 
 func loadSpec(cmd *cobra.Command) *api.BenchmarkSpec {
-	specFilePath, _ := cmd.Flags().GetString(ArgNameConfig)
+	specFilePath := GetString(cmd, ArgNameConfig)
 	spec, err := pkg.LoadSpec(specFilePath)
-	checkFatal(err)
+	CheckInitFatal(err)
 
 	return spec
 }
 
 func resolveReportHandler(cmd *cobra.Command, spec *api.BenchmarkSpec) (handler api.ReportHandler, err error) {
 	reportCtx := resolveReportContext(cmd)
-	outFile := resolveOutputFile(cmd)
+	outFile := ResolveOutputFileArg(cmd, ArgNameOutputFile)
 	writer := bufio.NewWriter(outFile)
 
-	switch reportFormat, _ := cmd.Flags().GetString(ArgNameFormat); reportFormat {
+	switch reportFormat := GetString(cmd, ArgNameFormat); reportFormat {
 	case ArgValueReportFormatMarkdownRaw:
 		streamReportWriter := internal.NewMarkdownStreamReportWriter(writer, reportCtx)
 		handler = pkg.NewStreamReportHandler(spec, reportCtx, streamReportWriter.Handle)
@@ -128,7 +87,7 @@ func resolveReportHandler(cmd *cobra.Command, spec *api.BenchmarkSpec) (handler 
 
 	case ArgValueReportFormatTxt:
 		var colorsOn = false
-		if file, _ := cmd.Flags().GetString(ArgNameOutputFile); file == "" {
+		if GetString(cmd, ArgNameOutputFile) == "" {
 			colorsOn = true
 		}
 
@@ -142,43 +101,15 @@ func resolveReportHandler(cmd *cobra.Command, spec *api.BenchmarkSpec) (handler 
 }
 
 func resolveReportContext(cmd *cobra.Command) *api.ReportContext {
-	labels, _ := cmd.Flags().GetStringSlice(ArgNameLabel)
-	includeHeaders, _ := cmd.Flags().GetBool(ArgNameHeaders)
-
 	return &api.ReportContext{
-		Labels:         labels,
-		IncludeHeaders: includeHeaders,
+		Labels:         GetStringSlice(cmd, ArgNameLabel),
+		IncludeHeaders: GetBool(cmd, ArgNameHeaders),
 	}
 }
 
 func resolveExecutionContext(cmd *cobra.Command, tracer api.Tracer) *api.ExecutionContext {
-	pipeStdOut, _ := cmd.Flags().GetBool(ArgNamePipeStdout)
-	pipeStdErr, _ := cmd.Flags().GetBool(ArgNamePipeStderr)
+	pipeStdOut := GetBool(cmd, ArgNamePipeStdout)
+	pipeStdErr := GetBool(cmd, ArgNamePipeStderr)
 
 	return api.NewExecutionContext(tracer, pkg.NewCommandExecutor(pipeStdOut, pipeStdErr))
-}
-
-func resolveOutputFile(cmd *cobra.Command) *os.File {
-	var outputFile = os.Stdout
-	var err error = nil
-
-	if outputFilePath, _ := cmd.Flags().GetString(ArgNameOutputFile); outputFilePath != "" {
-		resolvedfilePath := expandPath(outputFilePath)
-		outputFile, err = os.OpenFile(resolvedfilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	}
-	checkFatal(err)
-
-	return outputFile
-}
-
-// FIXME this has been copied from pgk/command_exec.go. Maybe share or use an existing implementation if exists.
-func expandPath(path string) string {
-	if strings.HasPrefix(path, "~") {
-		if p, err := os.UserHomeDir(); err == nil {
-			return filepath.Join(p, path[1:])
-		}
-		log.Warnf("Failed to resolve user home for path '%s'", path)
-	}
-
-	return path
 }
