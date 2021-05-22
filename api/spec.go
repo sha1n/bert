76cb6c1cf766af40297fb37894cf84940863d8d7
Shai Nagar <sha1n@users.noreply.github.com>
2021-05-22 12:45:27 +0300

    improved config validation

--- api/spec.go
+++ api/spec.go
@@ -20,7 +20,7 @@ type ScenarioSpec struct {
 
 // BenchmarkSpec benchmark specs top level structure
 type BenchmarkSpec struct {
-	Scenarios  []*ScenarioSpec `json:"scenarios" yaml:"scenarios" validate:"required,dive"`
+	Scenarios  []*ScenarioSpec `json:"scenarios" yaml:"scenarios" validate:"required,min=1,dive"`
 	Executions int             `validate:"required,gte=1"`
 	Alternate  bool
 }
