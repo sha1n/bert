fb3526b4f2438c9ac2d603b30b86e5deb4034612
Shai Nagar <sha1n@users.noreply.github.com>
2021-05-24 21:31:17 +0300

    improved test coverage (#51)

--- internal/cli/main_runner_test.go
+++ internal/cli/main_runner_test.go
@@ -16,7 +16,6 @@ import (
 
 var expectedGoVersionOutput string
 var itConfigFileArgValue = "--config=../../test/data/integration.yaml"
-var mainSourceFilePath = "../../cmd/main.go"
 
 func init() {
 	buf := new(bytes.Buffer)
@@ -44,6 +43,15 @@ func TestBasicMd(t *testing.T) {
 	assert.Contains(t, output, "|NAME|")
 	assert.Contains(t, output, expectedGoVersionOutput)
 }
+
+func TestBasicMdRaw(t *testing.T) {
+	output, err := runBenchmarkCommandWith(t, "-s", itConfigFileArgValue, "--format=md/raw")
+
+	assert.NoError(t, err)
+	assert.Contains(t, output, "|NAME|")
+	assert.Contains(t, output, expectedGoVersionOutput)
+}
+
 func TestBasicCsv(t *testing.T) {
 	output, err := runBenchmarkCommandWith(t, "-s", itConfigFileArgValue, "--format=csv")
 
@@ -52,16 +60,30 @@ func TestBasicCsv(t *testing.T) {
 	assert.Contains(t, output, expectedGoVersionOutput)
 }
 
+func TestBasicCsvRaw(t *testing.T) {
+	output, err := runBenchmarkCommandWith(t, "-s", itConfigFileArgValue, "--format=csv/raw")
+
+	assert.NoError(t, err)
+	assert.Contains(t, output, ",NAME,")
+	assert.Contains(t, output, expectedGoVersionOutput)
+}
+
+func TestWithMissingConfigFile(t *testing.T) {
+	nonExistingConfigArg := fmt.Sprintf("-c=/tmp/%s", test.RandomString())
+	_, _ = runBenchmarkCommandExpectPanic(t, "-s", nonExistingConfigArg)
+}
+
+func TestWithInvalidConfigFile(t *testing.T) {
+	invalidConfig := "-c=../../test/data/invalid_config.yml"
+	_, _ = runBenchmarkCommandExpectPanic(t, "-s", invalidConfig)
+}
+
+func TestWithCombinedDebugAndSilent(t *testing.T) {
+	_, _ = runBenchmarkCommandExpectPanic(t, "-s", "-d", itConfigFileArgValue)
+}
+
 func runBenchmarkCommandWith(t *testing.T, args ...string) (output string, err error) {
-	defer func() {
-		if o := recover(); o != nil {
-			if err, ok := o.(error); ok {
-				assert.NoError(t, err)
-			} else {
-				assert.NoError(t, fmt.Errorf("%v", o))
-			}
-		}
-	}()
+	defer expectNoPanic(t)
 
 	buf := new(bytes.Buffer)
 	writer := bufio.NewWriter(buf)
@@ -78,4 +100,47 @@ func runBenchmarkCommandWith(t *testing.T, args ...string) (output string, err e
 	err = rootCmd.Execute()
 
 	return buf.String(), err
+}
+
+func runBenchmarkCommandExpectPanic(t *testing.T, args ...string) (output string, err error) {
+	defer expectPanicWithError(t)
+
+	buf := new(bytes.Buffer)
+	writer := bufio.NewWriter(buf)
+
+	originalWriter := log.StandardLogger().Out
+	log.StandardLogger().SetOutput(buf)
+	defer log.StandardLogger().SetOutput(originalWriter)
+
+	rootCmd := NewRootCommand(test.RandomString(), test.RandomString(), test.RandomString())
+	rootCmd.SetArgs(args)
+	rootCmd.SetOut(writer)
+	rootCmd.SetErr(os.Stderr)
+
+	err = rootCmd.Execute()
+
+	return buf.String(), err
+}
+
+func expectNoPanic(t *testing.T) {
+	if o := recover(); o != nil {
+		if err, ok := o.(error); ok {
+			assert.NoError(t, err)
+		} else {
+			assert.NoError(t, fmt.Errorf("%v", o))
+		}
+	}
+}
+
+func expectPanicWithError(t *testing.T) {
+	if o := recover(); o != nil {
+		if err, ok := o.(error); ok {
+			assert.Error(t, err)
+		} else {
+			assert.Fail(t, fmt.Sprintf("A panic with an error was expected, but got: %v", o))
+		}
+	} else {
+		assert.Fail(t, "A panic with an error was expected, but got nothing")
+	}
+
 }
