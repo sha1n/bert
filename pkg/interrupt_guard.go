946a751033225009d1f2096af095be63b6f1aea7
sha1n <shai.nagar@gmail.com>
2021-06-19 14:39:29 +0300

    fixed incorrect notify channel config

--- pkg/interrupt_guard.go
+++ pkg/interrupt_guard.go
@@ -11,39 +11,13 @@ import (
 
 // RegisterInterruptGuard registers an interrupt signal handler and returns a cancellation function.
 func RegisterInterruptGuard(handleFn func(os.Signal)) context.CancelFunc {
-	c := make(chan os.Signal)
-	signal.Notify(c, os.Interrupt)
-
-	ctx, cancel := context.WithCancel(context.Background())
-
-	startWG := &sync.WaitGroup{}
-	startWG.Add(1)
-
-	go func() {
-		startWG.Done()
-
-		select {
-		case sig, ok := <-c:
-			if ok {
-				handleFn(sig)
-			}
-
-		case <-ctx.Done():
-			signal.Stop(c)
-
-			close(c)
-			log.Debug("Context cancelled - OK!")
-		}
-	}()
-
-	startWG.Wait()
-
+	cancel, _ := registerInterruptGuard(handleFn)
 	return cancel
 }
 
 // channel is returned for testing...
 func registerInterruptGuard(handleFn func(os.Signal)) (context.CancelFunc, chan os.Signal) {
-	c := make(chan os.Signal)
+	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 
 	ctx, cancel := context.WithCancel(context.Background())
