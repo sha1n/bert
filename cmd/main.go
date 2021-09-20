a3872d0303e028cdccd42add3e143c4201f9172c
Shai Nagar <sha1n@users.noreply.github.com>
2021-09-20 16:56:01 +0300

    added completions scripts support (#150)

--- cmd/main.go
+++ cmd/main.go
@@ -49,6 +49,7 @@ func doRun(exitFn func(int)) {
 
 	// Subcommands
 	rootCmd.AddCommand(cli.CreateConfigCommand(ctx))
+	rootCmd.AddCommand(cli.CreateCompletionCommand())
 	if enableSelfUpdate() {
 		rootCmd.AddCommand(cli.CreateUpdateCommand(Version, ProgramName, ctx))
 	}
